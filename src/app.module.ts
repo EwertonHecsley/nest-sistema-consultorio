0f265e30790d708c161a34c7f41da6a75d34f914
EwertonHecsley <ewerton.martinscomercial@gmail.com>
2024-03-11 16:38:43 -0300

    servico de hash

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from './database/database.module';
 import { ConsultorioModule } from './consultorio/consultorio.module';
+import { CryptografiaService } from './cryptografia/cryptografia.service';
 
 @Module({
   imports: [DatabaseModule, ConsultorioModule],
   controllers: [],
-  providers: [],
+  providers: [CryptografiaService],
 })
 export class AppModule { }
--- src/consultorio/consultorio.service.ts
+++ src/consultorio/consultorio.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { DatabaseService } from 'src/database/database.service';
+import { ConsultorioDto } from './dto/constultorio.dto';
 
 @Injectable()
 export class ConsultorioService {
     constructor(private readonly prisma: DatabaseService) { }
 
-    async buscarConsultorios() {
+    async buscarConsultorios(): Promise<ConsultorioDto[]> {
         return await this.prisma.consultorio.findMany();
     }
 }
