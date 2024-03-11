46c782c79299e7a70991e10b8b7256ff30f0cfe0
EwertonHecsley <ewerton.martinscomercial@gmail.com>
2024-03-11 16:46:11 -0300

    criar consultorio

--- src/consultorio/consultorio.module.ts
+++ src/consultorio/consultorio.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ConsultorioService } from './consultorio.service';
 import { ConsultorioController } from './consultorio.controller';
 import { DatabaseService } from 'src/database/database.service';
+import { CryptografiaService } from 'src/cryptografia/cryptografia.service';
 
 @Module({
-  providers: [ConsultorioService, DatabaseService],
+  providers: [ConsultorioService, DatabaseService, CryptografiaService],
   controllers: [ConsultorioController]
 })
 export class ConsultorioModule { }
--- src/consultorio/consultorio.service.ts
+++ src/consultorio/consultorio.service.ts
@@ -1,10 +1,29 @@
 import { Injectable } from '@nestjs/common';
 import { DatabaseService } from 'src/database/database.service';
 import { ConsultorioDto } from './dto/constultorio.dto';
+import { CryptografiaService } from 'src/cryptografia/cryptografia.service';
 
 @Injectable()
 export class ConsultorioService {
-    constructor(private readonly prisma: DatabaseService) { }
+    constructor(
+        private readonly prisma: DatabaseService,
+        private readonly hashService: CryptografiaService
+    ) { }
+
+    async criarConsultorio(data: ConsultorioDto): Promise<ConsultorioDto> {
+        const { nome, cnes, senha } = data;
+        const hashPassword = await this.hashService.hashPassword(senha);
+
+        const consultorio = await this.prisma.consultorio.create({
+            data: {
+                nome,
+                cnes,
+                senha: hashPassword
+            }
+        })
+
+        return consultorio;
+    }
 
     async buscarConsultorios(): Promise<ConsultorioDto[]> {
         return await this.prisma.consultorio.findMany();
