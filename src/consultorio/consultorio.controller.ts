f1f1460f60394dda71731d0b67f627200c0c7150
EwertonHecsley <ewerton.martinscomercial@gmail.com>
2024-03-11 16:49:36 -0300

    endpoint criar consultorio

--- src/consultorio/consultorio.controller.ts
+++ src/consultorio/consultorio.controller.ts
@@ -1,13 +1,22 @@
-import { Controller, Get, HttpStatus, Res } from '@nestjs/common';
+import { Body, Controller, Get, HttpStatus, Post, Res } from '@nestjs/common';
 import { ConsultorioService } from './consultorio.service';
 import { Response } from 'express';
+import { ConsultorioDto } from './dto/constultorio.dto';
 
 @Controller('consultorio')
 export class ConsultorioController {
     constructor(private readonly consultorioService: ConsultorioService) { }
 
+    @Post()
+    async create(@Body() data: ConsultorioDto, @Res() res: Response) {
+        const result = await this.consultorioService.criarConsultorio(data);
+
+        return res.status(HttpStatus.CREATED).json(result);
+
+    }
+
     @Get()
-    async listarConsultorios(@Res() res: Response) {
+    async listall(@Res() res: Response) {
         const result = await this.consultorioService.buscarConsultorios();
 
         return res.status(HttpStatus.OK).json(result);
